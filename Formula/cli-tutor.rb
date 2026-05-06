c0c22fcc1246e6c8088ed571b9100fece8ec2a2d
Shadab Ahmad Ghazali <shadab.ghazali@gmail.com>
2026-05-06 09:01:03 +0530

    chore: update Homebrew formula to v0.1.2

--- Formula/cli-tutor.rb
+++ Formula/cli-tutor.rb
@@ -1,8 +1,8 @@
 class CliTutor < Formula
   desc "Interactive terminal app for learning Unix command-line tools"
   homepage "https://github.com/ShadabGhazali/cli-tutor"
-  url "https://github.com/ShadabGhazali/cli-tutor/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "6234d48f8522c87f783d8032386b0201e40bc1ad966b6e93a034514b624e776f"
+  url "https://github.com/ShadabGhazali/cli-tutor/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "5afa1718337c1d958f08d3d9bc13daee0ec56805fe48faebd18c0bd6cbbc84f2"
   license "MIT"
 
   depends_on "rust" => :build
